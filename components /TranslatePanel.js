e0eed86e91acc3c3096a392d307139cb73ff0007
Healtime2025 <smoriti@gmail.com>
2025-05-23 23:42:41 +0200

    Update TranslatePanel.js

--- components /TranslatePanel.js
+++ components /TranslatePanel.js
@@ -1,1 +1,22 @@
+import React from 'react';
+
+export default function TranslatePanel({ input, onChange, onSend }) {
+  return (
+    <div className="flex gap-2 mt-4">
+      <input
+        type="text"
+        className="flex-1 border rounded p-2"
+        placeholder="Type your message..."
+        value={input}
+        onChange={onChange}
+      />
+      <button
+        onClick={onSend}
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+      >
+        Send
+      </button>
+    </div>
+  );
+}
 
