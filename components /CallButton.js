34ae605451d92ec47d0e83285130dcac7b31b3bd
Healtime2025 <smoriti@gmail.com>
2025-05-23 23:40:40 +0200

    Update CallButton.js

--- components /CallButton.js
+++ components /CallButton.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+export default function CallButton({ onCall }) {
+  return (
+    <button
+      onClick={onCall}
+      className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+    >
+      Call
+    </button>
+  );
+}
 
